cebc2abb1bc79e6e4db83762a90be7facd2c34d1
Vincent <leurart.vincent@outlook.be>
2019-12-08 19:47:52 +0100

    FINAL

--- ComputerHorizon/ComputerHorizon/Models/DataBase.cs
+++ ComputerHorizon/ComputerHorizon/Models/DataBase.cs
@@ -6,7 +6,7 @@ namespace ComputerHorizon.Models
     {
         //private static readonly string CONNECTION_STRING = @"Server=LAPTOP-58DH50DP;Database=Computer-Horizon;Integrated Security=SSPI;";
         private static readonly string CONNECTION_STRING = @"Server=VINCENT;Database=Computer-Horizon;Integrated Security=SSPI;";
-
+        //private static readonly string CONNECTION_STRING = @"Server=DAVID;Database=Computer-Horizon;Integrated Security=SSPI;";
         public static SqlConnection GetConnection()
         {
             return new SqlConnection(CONNECTION_STRING);
