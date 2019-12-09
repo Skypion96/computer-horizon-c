2e907a245f7117c2ec2d6af97979037cd155f15e
Vincent <leurart.vincent@outlook.be>
2019-12-09 14:28:42 +0100

    Ready for mobile

--- ComputerHorizon/ComputerHorizon/Startup.cs
+++ ComputerHorizon/ComputerHorizon/Startup.cs
@@ -52,6 +52,7 @@ namespace ComputerHorizon
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
             }
 
             app.UseStaticFiles(); //modifier
